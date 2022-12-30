19f67c1870bae91ce71af108dee57df79f77aee7
genaffleck <97855765+genaffleck@users.noreply.github.com>
2022-12-31 02:19:10 +0800

    Update control_statements.py

---
+++ control_statements.py
@@ -0,0 +1,16 @@
+#if, elif else
+condition = False
+name = "Roger"
+
+if condition == True:
+  print("The condition")
+  print("was true")
+elif name == "Roger":
+  print("Hello Roger")
+elif name == "Syd":
+  print("Hello Syd")
+elif name == "Flavio":
+  print("Hello Flavio")
+else:
+  print("The condition")
+  print("was false")
