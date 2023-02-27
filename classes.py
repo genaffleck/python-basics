ebc02c2e3b4739412bb7ee2991d8e7400c3aba2c
genaffleck <97855765+genaffleck@users.noreply.github.com>
2023-02-28 01:55:59 +0800

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,18 @@
+class Animal:
+    def walk(self):
+        print("Walking...")
+
+class Dog(Animal): #placing Animal inside parenthesis, Dog inherits from Animal Class
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print("woof")
+
+roger = Dog("Roger", 8)
+
+print(roger.name)
+print(roger.age)
+roger.bark()
+roger.walk()
